3c514306a7f39c5d1737f3405b33363f39c515d4
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2016-12-27 17:22:48 +0900

    RESULTTestクラスを作成

--- src/test/java/RESULTTest.java
+++ src/test/java/RESULTTest.java
@@ -1,13 +1,34 @@
+import com.sun.org.apache.regexp.internal.RE;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 /**
+ * 列挙型RESULTをテストするためのクラス
  * Created by sekiguchikai on 2016/12/27.
  */
 public class RESULTTest {
+    /**
+     * getResultメソッドをテストするためのメソッド
+     */
     @Test
-    public void getResult() throws Exception {
+    public void getResult() {
+        this.helper(RESULT.WIN, "君の勝ちだ");
+        this.helper(RESULT.LOSE, "君の負けだ");
+        this.helper(RESULT.DRAW, "引き分けだ");
+
+    }
+
+    /**
+     * getResultメソッドをテストするためのメソッドの補助メソッド
+     *
+     * @param data     テストで使用するデータ
+     * @param expected 期待する値
+     */
+    private void helper(RESULT data, String expected) {
+        assertThat(data.getResult(), is(expected));
 
     }
 
